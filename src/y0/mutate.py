e788ced969ea39deca593c3f258d7e10b352be87
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-13 13:26:24 +0100

    Update mutate.py

--- src/y0/mutate.py
+++ src/y0/mutate.py
@@ -6,11 +6,9 @@ from .dsl import Distribution, Fraction, P, Probability, Product
 
 
 def expand_simple_bayes(p: Probability) -> Fraction:
-    """Given P(Y|X,...), return P(X,...|Y)*P(Y)/P(X)."""
+    """Given P(Y|X,...), return P(X,...|Y)*P(Y)/P(X,...)."""
     if not p.distribution.is_markov_kernel():
         raise NotImplementedError
-
-    """P(Y|X1,X2) = P(X1,X2|Y)P(Y)/P(X1,X2)"""
 
     y = p.distribution.children
     x = p.distribution.parents
