68828e30b3d4bf5c568baadab7c1c417740b8ef3
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-13 13:25:49 +0100

    Update test_mutate.py

--- tests/test_mutate.py
+++ tests/test_mutate.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from y0.dsl import P, W, X, Y
+from y0.dsl import P, W, X, Y, Z
 from y0.mutate import expand_simple_bayes
 
 
@@ -15,6 +15,9 @@ class TestMutate(unittest.TestCase):
         """Test the expansion of a simple conditional to a Bayes-like expression."""
         self.assertEqual(P(X | Y) * P(Y) / P(X), expand_simple_bayes(P(Y | X)))
         self.assertEqual(P(X & W | Y) * P(Y) / P(X & W), expand_simple_bayes(P(Y | X | W)))
+
+        with self.assertRaises(NotImplementedError):
+            expand_simple_bayes(P(X & Y | Z))
 
     # def test_contract_bayes(self):
     #     """Test the reduction of a Bayes-like expression to a simple conditional."""
