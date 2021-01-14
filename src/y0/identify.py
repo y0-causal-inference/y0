498055e34041ba642dd50d8b2d804eba6094ba1b
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-14 18:18:36 +0100

    Add next step for is_id algorithm

--- src/y0/identify.py
+++ src/y0/identify.py
@@ -2,15 +2,33 @@
 
 """Implementations of the identify algorithm from Shpitser and Pearl."""
 
-from ananke.graphs import ADMG
+from typing import List, Set
 
-from .dsl import Expression
+from ananke.graphs import ADMG
+from ananke.identification import OneLineID
+
+from .dsl import Expression, Variable
 
 __all__ = [
     'is_identifiable',
 ]
 
 
+def _get_treatments(variables: Set[Variable]) -> List[str]:
+    raise NotImplementedError
+
+
+def _get_outcomes(variables: Set[Variable]) -> List[str]:
+    raise NotImplementedError
+
+
 def is_identifiable(graph: ADMG, query: Expression) -> bool:
     """Check if the expression is identifiable."""
-    raise NotImplementedError
+    query_variables = query.get_variables()
+
+    one_line_id = OneLineID(
+        graph=graph,
+        treatments=_get_treatments(query_variables),
+        outcomes=_get_outcomes(query_variables),
+    )
+    return one_line_id.id()
