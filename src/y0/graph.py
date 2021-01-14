b70cf4e519f874569a170581e9aa070ec8de0f00
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-14 19:26:45 +0100

    Change variable name

--- src/y0/graph.py
+++ src/y0/graph.py
@@ -8,19 +8,19 @@ import networkx as nx
 from ananke.graphs import ADMG
 
 __all__ = [
-    'NxMixedGraph',
+    'ADMGBuilder',
 ]
 
 
 @dataclass
-class NxMixedGraph:
+class ADMGBuilder:
     """A mixed graph based on a :class:`networkx.Graph` and a :class:`networkx.DiGraph`.
 
     Example usage:
 
     .. code-block:: python
 
-        graph = NxMixedGraph()
+        graph = ADMGBuilder()
         graph.add_directed_edge('X', 'Y')
         graph.add_undirected_edge('X', 'Y')
         admg_graph = graph.to_admg()
@@ -31,13 +31,13 @@ class NxMixedGraph:
     #: A undirected graph
     undirected: nx.Graph = field(default_factory=nx.Graph)
 
-    def add_directed_edge(self, u, v, **attr):
+    def add_directed_edge(self, u, v, **attr) -> None:
         """Add a directed edge from u to v."""
         self.directed.add_edge(u, v, **attr)
         self.undirected.add_node(u)
         self.undirected.add_node(v)
 
-    def add_undirected_edge(self, u, v, **attr):
+    def add_undirected_edge(self, u, v, **attr) -> None:
         """Add an undirected edge between u and v."""
         self.undirected.add_edge(u, v, **attr)
         self.directed.add_node(u)
