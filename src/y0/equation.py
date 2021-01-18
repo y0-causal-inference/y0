5e6b974b78443419ed46af20b295a2b1589b61ad
Cottam, Joseph A <joseph.cottam@pnnl.gov>
2021-01-17 23:29:41 -0800

    Simplifying the metaclass stuff by being a little opinionated.

--- src/y0/equation.py
+++ src/y0/equation.py
@@ -1,7 +1,11 @@
 import inspect
 from y0.dsl import Variable, _Mathable
 
-#Liberally borrowing from https://github.com/drhagen/parsita/blob/master/src/parsita/metaclasses.py
+#Somewhat based on  https://github.com/drhagen/parsita/blob/master/src/parsita/metaclasses.py
+#TODO: Inheret from ABC to ensure that 'eq' exists.  
+#TODO: Make Equation a subclass of _Mathable.  This will require monkeying with _Mathable so the metatypes line up properly.
+#TODO: Can you grab the body of a class and wrap it in an assignment?  If so, we could remove a bit of boilerplate!
+
 class _MakeVariables(dict):
     def __missing__(self, key):
         class_body_globals = inspect.currentframe().f_back.f_globals
@@ -14,28 +18,14 @@ class EquationMeta(type):
     @classmethod
     def __prepare__(mcs, name, bases, **_):  # noqa: N804
         return _MakeVariables()
-    
-    def _default_value(cls):
-        keys = [k for k in vars(cls).keys() 
-               if not k.startswith("_")]
-        if len(keys) > 0:
-            return vars(cls)[keys[0]]
-        else:
-            raise ValueError("No default Value")
-    
-    def __repr__(cls):
-        try: return str(cls._default_value())
-        except ValueError: return super().__repr__()     
         
-    def _repr_latex_(cls):
-        try: return f'${cls._default_value().to_latex()}$'
-        except ValueError: return super().__repr__()
+    def __repr__(cls): return str(vars(cls)["eq"])
+        
+    def _repr_latex_(cls): return f'${vars(cls)["eq"].to_latex()}$'
     
     def __call__(cls, *args, **kwargs):
         raise TypeError('`Equation` class cannot be instantiated. It is used to provide automatic forward declarations '
                         'for variables. Access the individual variables/equations as static attributes.')
 
-class Equation(metaclass=EquationMeta):
-    def __new__(mcs, name, bases, dct, **_):  # noqa: N804
-        return super().__new__(mcs, name, bases, dct)
+class Equation(metaclass=EquationMeta): pass
   
