9f1a019dfe95fc843ea621d74885db185c733b65
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-23 13:12:06 +0100

    Clean up tests

--- tests/test_algorithm/test_get_conditional_independencies.py
+++ tests/test_algorithm/test_get_conditional_independencies.py
@@ -3,27 +3,32 @@
 """Test getting conditional independencies."""
 
 import unittest
-from typing import Set
 from itertools import chain
+from typing import Set
+
+from ananke.graphs import ADMG
 
 from y0.algorithm import falsification
-
 from y0.algorithm.conditional_independencies import ConditionalIndependency, get_conditional_independencies
+from y0.examples import examples
+from y0.graph import NxMixedGraph
 
-from y0.graph import NxMixedGraph, ADMG #, napkin_graph
 
 class TestDSeparation(unittest.TestCase):
     "Test the d-separation utility."
-    #TODO: Migrate to the ADMG representation, not just vanilla networkx
+
+    # TODO: Migrate to the ADMG representation, not just vanilla networkx
     def test_mit_example(self):
         # Test graph and cases from http://web.mit.edu/jmn/www/6.034/d-separation.pdf
-        edges = [("AA","C"), ("B","C"), ("C","D"), ("C","E"), ("D","F"), ("F","G")]
-        #layout = {"AA": (0,-1), "B": (2,-1), "C": (1,-2), "D": (0, -3), 
+        edges = [("AA", "C"), ("B", "C"), ("C", "D"), ("C", "E"), ("D", "F"), ("F", "G")]
+        # layout = {"AA": (0,-1), "B": (2,-1), "C": (1,-2), "D": (0, -3),
         #          "E": (2, -3), "F":(1,-4), "G": (0,-5)}
         G = ADMG()
-        for v in chain(*edges): G.add_vertex(v)
-        for edge in edges: G.add_diedge(*edge)
-                                               
+        for v in chain(*edges):
+            G.add_vertex(v)
+        for edge in edges:
+            G.add_diedge(*edge)
+
         self.assertFalse(falsification.are_d_separated(G, "AA", "B", given=["D", "F"]))
         self.assertTrue(falsification.are_d_separated(G, "AA", "B"))
         self.assertTrue(falsification.are_d_separated(G, "D", "E", given=["C"]))
@@ -31,7 +36,8 @@ class TestDSeparation(unittest.TestCase):
         self.assertFalse(falsification.are_d_separated(G, "D", "E"))
         self.assertFalse(falsification.are_d_separated(G, "D", "E", given=["AA", "B"]))
         self.assertFalse(falsification.are_d_separated(G, "G", "G", given=["C"]))
-        
+
+
 class TestGetConditionalIndependencies(unittest.TestCase):
     """Test getting conditional independencies."""
 
@@ -45,10 +51,13 @@ class TestGetConditionalIndependencies(unittest.TestCase):
             ),
             msg='one or more of the returned ConditionalIndependency instances are not canonical',
         )
-        self.assertEqual(expected, conditional_independencies)
+        self.assertEqual(set(expected), set(conditional_independencies))
 
-    def test_napkin(self):
-        """Test getting the conditional independencies from the napkin graph."""
-        d1 = ConditionalIndependency('R', 'V1', ('W',))
-        d2 = ConditionalIndependency('X', 'V1', ('W',))
-        self.assert_conditional_indepencencies(napkin_graph, {d1, d2})
+    def test_examples(self):
+        """Test getting the conditional independencies from the example graphs."""
+        for example in examples:
+            with self.subTest(name=example.name):
+                self.assert_conditional_indepencencies(
+                    graph=example.graph,
+                    expected=example.conditional_independencies,
+                )
