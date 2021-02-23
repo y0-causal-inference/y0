3b42b3489db4697a9cdc002a8a25c1d5bcc40d54
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-23 20:01:46 +0100

    Make classes for results reusable

--- src/y0/algorithm/falsification.py
+++ src/y0/algorithm/falsification.py
@@ -1,35 +1,43 @@
-from ..util import CITests
+import copy
+from collections import abc
+from itertools import chain, combinations
+
 import networkx as nx
 import pandas as pd
-
-from collections import abc
-from itertools import combinations, chain
 from tqdm import tqdm
-import copy
+
+from ..util import CITests
 
 
-def are_d_separated(G, a, b, *, given=frozenset()):
+class Result:
+    """By default, acts like a boolean, but also caries evidence graph."""
+
+    def __init__(self, separated, a, b, given, evidence):
+        """separated -- T/F judgement
+           a/b/given -- The question asked
+           evidence -- The end graph
+        """
+        self.separated = separated
+        self.a = a
+        self.b = b
+        self.given = given
+        self.evidence = evidence
+
+    def __bool__(self):
+        return self.separated
+
+    def __repr__(self):
+        return f"{repr(self.separated)} ('{self.a}' d-sep '{self.b}' given {self.given})"
+
+    def __eq__(self, other):
+        return self.separated == other
+
+
+def are_d_separated(G, a, b, *, given=frozenset()) -> Result:
     """Tests if nodes named by a & b are d-separated in G.
     Given conditions can be provided with the optional 'given' parameter.
     returns T/F and the final graph (as evidence)
     """
-    class Result:
-        """By default, acts like a boolean, but also caries evidence graph."""
-        def __init__(self, separated, a, b, given, evidence):
-            """separated -- T/F judgement
-               a/b/given -- The question asked
-               evidence -- The end graph
-            """
-            self.separated = separated
-            self.a = a
-            self.b = b
-            self.given = given
-            self.evidence = evidence
-
-        def __bool__(self): return self.separated  # noqa: E704
-        def __repr__(self): return f"{repr(self.separated)} ('{a}' d-sep '{b}' given {given})"  # noqa: E704
-        def __eq__(self, other): return self.separated == other  # noqa: E704
-
     named = set([a, b]).union(given)
 
     # Filter to ancestors
@@ -67,29 +75,35 @@ def all_combinations(source, min=0, max=None):
     return [*chain(*(combinations(source, size) for size in range(min, max + 1)))]
 
 
-def falsifications(G, df, significance_level=.05, max_given=None, verbose=False):
+class Evidence(abc.Sequence):
+    """A list of variables pairs that failed the d_sepration+covariance test.
+
+    Has an extra 'evidence' property that is a dictionary.
+      * Keys are the d-separated variable pairs
+      * Values are the covariances measured between them.
+
+    verbose -- print each combination as its checked
+    max_given -- Longest list of 'given' variables to check
+    """
+
+    def __init__(self, failures, evidence):
+        self._failures = failures
+        self.evidence = evidence
+
+    def __getitem__(self, i):
+        return self._failures[i]
+
+    def __len__(self):
+        return len(self._failures)
+
+    def __repr__(self):
+        return repr(self._failures) + "+evidence"
+
+
+def falsifications(G, df, significance_level=.05, max_given=None, verbose=False) -> Evidence:
     # TODO: Take G, [ConditionalIndependency...], df, etc. as params
     #       Test independencies passed
     # TODO: Make function G -> [ConditionalIndpeendency...]
-
-    class Evidence(abc.Sequence):
-        """A list of variables pairs that failed the d_sepration+covariance test.
-
-        Has an extra 'evidence' property that is a dictionary.
-          * Keys are the d-separated variable pairs
-          * Values are the covariances measured between them.
-
-        verbose -- print each combination as its checked
-        max_given -- Longest list of 'given' variables to check
-        """
-        def __init__(self, failures, evidence):
-            self._failures = failures
-            self.evidence = evidence
-
-        def __getitem__(self, i): return self._failures[i]  # noqa: E704
-        def __len__(self): return len(self._failures)  # noqa: E704
-        def __repr__(self): return repr(self._failures) + "+evidence"  # noqa: E
-
     all_nodes = set(G.vertices)
     all_pairs = combinations(all_nodes, 2)
 
