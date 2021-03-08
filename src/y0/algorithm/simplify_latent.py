d1e78e73d334a4e566b71ac72b3e2c3c8e41df90
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-08 21:24:40 +0100

    Update simplify_latent.py

--- src/y0/algorithm/simplify_latent.py
+++ src/y0/algorithm/simplify_latent.py
@@ -2,27 +2,40 @@
 
 """Implement Robin Evans' simplification algorithms."""
 
-from collections import defaultdict
-
 import itertools as itt
+from collections import defaultdict
+from typing import DefaultDict, Iterable, List, Mapping, Optional, Set, Tuple
+
 import networkx as nx
-from typing import Iterable, Mapping, Optional, Set, Tuple
+
+__all__ = [
+    'remove_widow_latents',
+    'transform_latents_with_parents',
+    'remove_redundant_latents',
+]
 
 DEFAULT_TAG = 'latent'
 
 
-def remove_widow_latents(graph: nx.DiGraph, tag: Optional[str] = None) -> nx.DiGraph:
+def remove_widow_latents(graph: nx.DiGraph, tag: Optional[str] = None) -> Tuple[nx.DiGraph, Set[str]]:
     """Remove latents with no children (in-place).
 
-    :param graph:
-    :param tag:
-    :return:
+    :param graph: A latent variable DAG
+    :param tag: The tag for which variables are latent
+    :returns: The graph, modified in place
     """
-    graph.remove_nodes_from(list(iter_widow_latents(graph, tag=tag)))
-    return graph
+    remove = set(iter_widow_latents(graph, tag=tag))
+    graph.remove_nodes_from(remove)
+    return graph, remove
 
 
 def iter_widow_latents(graph: nx.DiGraph, *, tag: Optional[str] = None) -> Iterable[str]:
+    """Iterate over latent variables with no children.
+
+    :param graph: A latent variable DAG
+    :param tag: The tag for which variables are latent
+    :yields: Nodes with no children
+    """
     if tag is None:
         tag = DEFAULT_TAG
     for node, data in graph.nodes.items():
@@ -35,13 +48,19 @@ def transform_latents_with_parents(
     tag: Optional[str] = None,
     suffix: str = '_PRIME',
 ) -> nx.DiGraph:
-    """Transform latent variables with parents into latent variables with no parents."""
+    """Transform latent variables with parents into latent variables with no parents.
+
+    :param graph: A latent variable DAG
+    :param tag: The tag for which variables are latent
+    :param suffix: The suffix to postpend to transformed latent variables.
+    :returns: The graph, modified in place
+    """
     if tag is None:
         tag = DEFAULT_TAG
 
-    remove_nodes = []
-    add_edges = []
-    modified_latents = defaultdict(list)
+    remove_nodes: List[str] = []
+    add_edges: List[Tuple[str, str]] = []
+    modified_latents: DefaultDict[str, List[str]] = defaultdict(list)
     for latent_node, parents, children in iter_middle_latents(graph, tag=tag):
         remove_nodes.append(latent_node)
         add_edges.extend(itt.product(parents, children))
@@ -64,7 +83,12 @@ def _add_modified_latent(graph: nx.DiGraph, modified_latent: Mapping[str, Iterab
 
 
 def iter_middle_latents(graph: nx.DiGraph, *, tag: Optional[str] = None) -> Iterable[Tuple[str, Set[str], Set[str]]]:
-    """Iterate over latent nodes that have both parents and children (along with them)."""
+    """Iterate over latent nodes that have both parents and children (along with them).
+
+    :param graph: A latent variable DAG
+    :param tag: The tag for which variables are latent
+    :yields: Nodes with both parents and children, along with the set of parents and set of children
+    """
     if tag is None:
         tag = DEFAULT_TAG
     for node, data in graph.nodes.items():
@@ -79,28 +103,30 @@ def iter_middle_latents(graph: nx.DiGraph, *, tag: Optional[str] = None) -> Iter
         yield node, parents, children
 
 
-def simplification_3(
-    graph: nx.DiGraph,
-    tag: Optional[str] = None,
-    inplace: bool = False,
-) -> nx.DiGraph:
-    """Transform"""
+def remove_redundant_latents(graph: nx.DiGraph, tag: Optional[str] = None) -> Tuple[nx.DiGraph, Set[str]]:
+    """Remove redundant latent variables.
+
+    :param graph: A latent variable DAG
+    :param tag: The tag for which variables are latent
+    :returns: The graph, modified in place
+    """
+    remove = set(_iter_redundant_latents(graph, tag=tag))
+    graph.remove_nodes_from(remove)
+    return graph, remove
+
+
+def _iter_redundant_latents(graph, *, tag: Optional[str] = None) -> Iterable[str]:
     if tag is None:
         tag = DEFAULT_TAG
-    if not inplace:
-        graph = graph.copy()
     latents: Mapping[str, Set[str]] = {
         node: {child for _, child in graph.out_edges(node)}
         for node, data in graph.nodes.items()
         if data[tag]
     }
-    remove = []
-    for (left, left_children), (right, right_children) in itt.product(latents.items(), r=2):
+    for (left, left_children), (right, right_children) in itt.product(latents.items(), repeat=2):
         if left_children == right_children and left > right:
             # if children are the same, keep the lower sort order node
-            remove.append(left)
+            yield left
         elif left_children < right_children:
             # if left's children are a proper subset of right's children, we don't need left
-            remove.append(left)
-    graph.remove_nodes_from(remove)
-    return graph
+            yield left
