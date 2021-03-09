f84736f74653b4433d9c3721c68e94020faeabb1
Cottam, Joseph A <joseph.cottam@pnnl.gov>
2021-03-09 09:01:26 -0800

    Testing alternative interface

--- tests/test_algorithm/test_falsification.py
+++ tests/test_algorithm/test_falsification.py
@@ -5,16 +5,27 @@
 import unittest
 
 from y0.algorithm.falsification import falsifications
+from y0.algorithm.conditional_independencies import get_conditional_independencies
+
 from y0.examples import asia_example
 
 
 class TestFalsification(unittest.TestCase):
     "Are falsifiable implications testable?"
 
-    def test_asia(self):
+    def test_asia_from_graph(self):
         "Test the asia graph against data generated from it."
         G = asia_example.graph.to_admg()
         df = asia_example.data
         issues = falsifications(G, df)
         self.assertEqual(0, len(issues))
         self.assertGreater(len(issues.evidence), 0)
+
+    def test_asia_from_list(self):
+        "Test the asia graph against data generated from it, passing in the implications to test."
+        G = asia_example.graph.to_admg()
+        df = asia_example.data
+        implications = get_conditional_independencies(G)
+        issues = falsifications(implications, df)
+        self.assertEqual(0, len(issues))
+        self.assertEqual(len(issues.evidence), len(implications))
