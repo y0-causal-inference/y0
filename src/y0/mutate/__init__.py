4f2c5606fd5a6d0433c99dd7d8c01b295d5e7398
Jeremy Zucker <djinnome@gmail.com>
2021-05-14 16:10:17 -0700

    Added conditional to fraction inside mutate

--- src/y0/mutate/__init__.py
+++ src/y0/mutate/__init__.py
@@ -5,5 +5,10 @@
 from .canonicalize_expr import canonicalize
 
 __all__ = [
-    'canonicalize',
+    'canonicalize','conditional_to_fraction'  
 ]
+
+def conditional_to_fraction( a: Probability ):
+    if a.distribution.is_conditioned():
+        children, parents = a.distribution.children, a.distribution.parents
+        return P(children + parents )/Sum[parents](P(children + parents ))
