a6215ac2b8b62b4b347ce998044687ed215df616
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-06-22 17:28:27 -0400

    Update id_c.py

--- src/y0/algorithm/identify/id_c.py
+++ src/y0/algorithm/identify/id_c.py
@@ -1,8 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Implementation of the IDC algorithm."""
+
 from .id_std import identify
 from ..conditional_independencies import are_d_separated
+from ...dsl import Expression
+from ...graph import NxMixedGraph
 
 
-def idx(outcomes, treatments, conditions, graph):
+def idx(outcomes, treatments, conditions, graph: NxMixedGraph[str]) -> Expression:
+    """Run the IDC algorithm.
+
+    :param outcomes:
+    :param treatments:
+    :param conditions:
+    :param graph:
+    """
     if idc_condition():
         ...
         return idx(...)
@@ -12,7 +25,9 @@ def idx(outcomes, treatments, conditions, graph):
     return ...
 
 
-def idc_condition(graph, conditions, outcomes):
+def idc_condition(graph: NxMixedGraph[str], conditions, outcomes) -> bool:
+    """Check the IDC condition for recursion."""
+    admg = graph.to_admg()
     for condition in conditions:
         if are_d_separated(outcomes, ...):
             ...
