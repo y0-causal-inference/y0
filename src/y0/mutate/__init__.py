c75988916a3f0d962c2fc3be97ea45020c7cccb1
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-06-25 11:07:24 -0400

    Add canonicalized expression equals test (#66)
    * Add implementation and tests
    
    * Rename
    
    * Fix order

--- src/y0/mutate/__init__.py
+++ src/y0/mutate/__init__.py
@@ -2,11 +2,12 @@
 
 """Functions that mutate probability expressions."""
 
-from .canonicalize_expr import canonicalize
+from .canonicalize_expr import canonical_expr_equal, canonicalize
 from .chain import bayes_expand, chain_expand, fraction_expand
 
 __all__ = [
     "canonicalize",
+    "canonical_expr_equal",
     "chain_expand",
     "fraction_expand",
     "bayes_expand",
