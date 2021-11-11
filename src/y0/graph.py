234ca5edc0322450b5eba0341709945e92745224
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-11 09:33:58 +0100

    Update graph.py

--- src/y0/graph.py
+++ src/y0/graph.py
@@ -431,7 +431,7 @@ def _include_adjacent(
 
 def _exclude_source(
     graph: nx.Graph, vertices: set[Variable]
-) -> Collection[Tuple[Variable, Variable]]:d
+) -> Collection[Tuple[Variable, Variable]]:
     return [(u, v) for u, v in graph.edges() if u not in vertices]
 
 
