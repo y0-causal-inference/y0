e67ab77ce8f7af577cfd46893604a71317a7f9e3
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-12-02 09:55:39 -0500

    Renaming and add base implementation fr variables/probability

--- src/y0/complexity.py
+++ src/y0/complexity.py
@@ -5,8 +5,10 @@
 from typing import Sequence
 
 from .dsl import (
+    CounterfactualVariable,
     Expression,
     Fraction,
+    Intervention,
     One,
     Probability,
     Product,
@@ -26,10 +28,15 @@ FRAC_CONST = 0.0
 PROD_CONST = 0.0
 PROB_CONST = 0.0
 Q_CONST = 0.0
+VAR_CONSTANT = 1.0  # yo cat
 
 
 def complexity(expr: Expression) -> float:
-    """Calculate the complexity of the given expression, recursively."""
+    """Calculate the "complexity" of the expression, where a bigger result means more complex.
+
+
+    :param expr: The input expression (can be any DSL element except
+    """
     if isinstance(expr, (One, Zero)):
         return CONST_CONST
     if isinstance(expr, Fraction):
@@ -41,19 +48,25 @@ def complexity(expr: Expression) -> float:
     if isinstance(expr, QFactor):
         return Q_CONST + range_complexity(expr.domain) + range_complexity(expr.codomain)
     if isinstance(expr, Probability):
-        return PROB_CONST + prob_complexity(expr)
+        return PROB_CONST + probability_complexity(expr)
     raise TypeError(f"Unhandled expression type: {expr.__class__.__name__}")
 
 
 def range_complexity(variables: Sequence[Variable]) -> float:
     """Return the complexity of a sequence of variables."""
-    return sum(var_complexity(v) for v in variables)
+    return sum(variable_complexity(v) for v in variables)
 
 
-def var_complexity(variable: Variable) -> float:
+def variable_complexity(variable: Variable) -> float:
     """Return the complexity of a single variable."""
-    return 1.0  # FIXME
+    if isinstance(variable, CounterfactualVariable):
+        return VAR_CONSTANT + sum(variable_complexity(v) for v in variable.interventions)
+    elif isinstance(variable, Intervention):
+        return VAR_CONSTANT
+    else:
+        return VAR_CONSTANT
 
 
-def prob_complexity(expr: Probability) -> float:
-    raise NotImplementedError
+def probability_complexity(expr: Probability) -> float:
+    """Return the complexity of a probability expression."""
+    return range_complexity(expr.children) + range_complexity(expr.parents)
