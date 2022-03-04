89f81588af44ffb35b14d2ee43b17e5bc914a7bb
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-03-04 19:30:58 +0100

    Add additional rules for counterfactuals inside conditions

--- tests/test_dsl.py
+++ tests/test_dsl.py
@@ -139,7 +139,7 @@ class TestDSL(unittest.TestCase):
             (P(+Y @ -X), "P(+Y @ -X)"),
             (P(+Y @ ~X), "P(+Y @ +X)"),
             (P(+Y @ +X), "P(+Y @ +X)"),
-            #
+            # Interventions can live inside the conditions
             (P(Y @ X | ~X, ~Y), "P(Y @ -X | +X, +Y)"),
             (P(Y @ -X | ~X, ~Y), "P(Y @ -X | +X, +Y)"),
             (P(Y @ +X | ~X, ~Y), "P(Y @ +X | +X, +Y)"),
@@ -147,6 +147,11 @@ class TestDSL(unittest.TestCase):
             #
             (P(~(Y @ ~X) | X, Y), "P(+Y @ +X | X, Y)"),
             (P(~Y @ ~X | X, Y), "P(+Y @ +X | X, Y)"),  # should be same as above
+            # Counterfactuals can live inside the conditions
+            (P(Y @ X | ~X, Z @ D, D), "P(Y @ -X | D, +X, Z @ -D)"),
+            (P(Y @ X | ~X, Z @ D, -D), "P(Y @ -X | -D, +X, Z @ -D)"),
+            (P(Y @ X | ~X, Z @ D, +D), "P(Y @ -X | +D, +X, Z @ -D)"),
+            (P(Y @ X | ~X, Z @ D, ~D), "P(Y @ -X | +D, +X, Z @ -D)"),
         ]:
             with self.subTest(expr=expected):
                 self.assert_exp(expr, expected)
