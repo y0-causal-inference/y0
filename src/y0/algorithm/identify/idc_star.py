8aded13a1a5b47b574050457738bf0491126b051
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-03-17 18:51:52 +0100

    Add ICD star code

---
+++ src/y0/algorithm/identify/idc_star.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+"""Implementation of the ``IDC*`` algorithm."""
+
+from .cg import make_counterfactual_graph
+from .id_star import id_star
+from .utils import Unidentifiable
+from ..conditional_independencies import are_d_separated
+from ...dsl import Event, Expression, P, Zero
+from ...graph import NxMixedGraph
+
+__all__ = [
+    "idc_star",
+]
+
+
+# FIXME replace with :class:`y0.algorithms.identify.utils.Unidentifiable`
+class Inconsistent(ValueError):
+    pass
+
+
+def get_delta_event(event: Event) -> Event:
+    """Return a reduced event that doesn't have any conditionals in it."""
+    raise NotImplementedError
+
+
+def is_event_conditional(query: Event) -> bool:
+    """Return if a counterfactual event is conditional."""
+    raise NotImplementedError
+
+
+def idc_star(graph: NxMixedGraph, query: Event) -> Expression:
+    r"""Run the ``IDC*`` algorithm.
+
+    INPUT:
+        G a causal diagram,
+        :math:`\event` a conjunction of counterfactual outcomes,
+        :math:`\delta` a conjunction of counterfactual observations
+    :returns: an expression for :math:`P(\event | \delta)` in terms of P, FAIL, or UNDEFINED
+    """
+    if not is_event_conditional(query):
+        raise ValueError(f"Query {query} must be conditional")
+
+    delta = get_delta_event(query)
+
+    # Line 1
+    try:
+        id_star(graph, delta)
+    except Unidentifiable:
+        raise Unidentifiable
+
+    event = set(query.children)
+    # Line 2:
+    try:
+        new_graph, new_query = make_counterfactual_graph(graph, P(event.union(delta)))
+        new_event = {g for g in event if g in new_query.children}
+        new_delta = {d for d in delta if d in new_query.children}
+        vertices = set(new_graph.nodes())
+    # Line 3:
+    except Inconsistent:
+        # (f"query {event.union(delta)} is inconsistent")
+        return Zero()
+    # Line 4:
+    for counterfactual in new_delta:
+        # TODO do we need to extend the notion of d-separation from 1-1 to 1-many?
+        if are_d_separated(new_graph.remove_out_edges(counterfactual), counterfactual, new_event):
+            counterfactual_value = counterfactual.parent()
+            parents = new_delta - {counterfactual}
+            children = {g.remove_in_edges(counterfactual_value) for g in new_event}
+            return idc_star(graph, P(children | parents))
+    # Line 5:
+    estimand = id_star(graph, new_query)
+    return estimand.marginalize(vertices - delta)
+
+
+def idc_star_line_2(graph: NxMixedGraph, query: Event) -> Expression:
+    r"""Run line 2 of the ``IDC*`` algorithm.
+
+    The second line states that if :math:`\event` contains a counterfactual which violates
+    the Axiom of Effectiveness (Pearl, 2000), then :math:`\event` is inconsistent, and we
+    return probability 0.
+    """
+    delta = query.parents
+    event_and_delta = query.uncondition()
+    return make_counterfactual_graph(graph, event_and_delta)
+
+
+def idc_star_line_4(graph: NxMixedGraph, query: Event) -> bool:
+    r"""Run line 4 of the ``IDC*`` algorithm.
+
+    Line 4 of IDC* is the central line of the algorithm and is
+    analogous to line 1 of IDC. In IDC, we moved a value
+    assignment :math:`Z = z` from being observed to being fixed if
+    there were no back-door paths from :math:`Z` to the outcome
+    variables :math:`Y` given the context of the effect of
+    :math:`do(\mathbf{x})`. Here in IDC*, we move a counterfactual
+    value assignment :math:`Y_\mathbf{x} = y` from being observed (that is being a
+    part of :math:`\delta`), to being fixed (that is appearing in every
+    subscript of :math:`\event'` ) if there are no back-door paths from :math:`Y_\mathbf{x}` to
+    the counterfactual of interest :math:`\event'` .
+    """
+    event = set(query.children)
+    raise NotImplementedError
---
+++ tests/test_algorithm/test_idc_star.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for the ``IDC*`` algorithm."""
+
+from tests.test_algorithm import cases
+from y0.algorithm.identify.idc_star import idc_star_line_2
+from y0.dsl import D, P, Variable, W, X, Y, Z
+from y0.examples import figure_9a, figure_9c
+
+d, w, x, y, z = -D, -W, -X, -Y, -Z
+
+
+class TestIDCStar(cases.GraphTestCase):
+    """Tests for the ``IDC*`` algorithm."""
+
+    def test_idc_star_line_2(self):
+        r"""Test line 2 of the IDC* algorithm.
+
+        Construct the counterfactual graph Figure 9(c) where the corresponding modified query
+        is :math:`P(Y_{x} = y|X= x',Z=z,D=d)`
+        """
+        input_event = {Y @ -x: x}
+        input_conditional = {X: +x, Z: -z, D: -d}
+        input_graph = figure_9a.graph
+        actual_graph, actual_query = idc_star_line_2(input_graph, input_event, input_conditional)
+        expected_graph = figure_9c.graph
+        expected_query = P(D, X, Y @ ~X, Z)
+        self.assert_expr_equal(expected=expected_query, actual=actual_query)
+        self.assert_graph_equal(expected=expected_graph, actual=actual_graph)
+
+    def test_idc_star_line_4(self):
+        r"""Test line 4 of the IDC* algorithm.
+
+        Check that line 4 or IDC* works correctly moves :math:`Z, D` (with
+        :math:`D` being redundant due to graph structure) to the
+        subscript of :math:`Y_\mathbf{x}`, to obtain :math:`P(Y_{X',Z} | X )`,
+        and calls IDC* with this query recursively.
+        """
+        input_query = P(Y @ ~X | X, Z, D)
+        expected_output_query = P(Y @ (~X, Z) | X)
+        new_delta = {X, Z, D}
+        new_event = {Y @ ~X}
+        graph = figure_9c.graph
+        for counterfactual in [Z, D]:
+            # self.assertTrue(are_d_separated(graph.remove_outgoing_edges_from( {counterfactual} ), counterfactual, new_event))
+            counterfactual_value = Variable(counterfactual.name)
+            parents = new_delta - {counterfactual}
+            children = {g.intervene(counterfactual_value) for g in new_event}
+            # self.assert_expr_equal( P( Y @ {X, counterfactual}  | new_event - {counterfactual}), P(children | parents))
