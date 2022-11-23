86464273afb951efe59129aad11372c7c58a8427
Jeremy Zucker <djinnome@gmail.com>
2022-11-23 12:44:29 -0500

    Added unit tests for Allen Interval relations

--- src/y0/dedri.py
+++ src/y0/dedri.py
@@ -1,4 +1,9 @@
-from y0.dsl import Element, Variable, Intervention, CounterfactualVariable    
+from y0.dsl import Element, Variable, Intervention, CounterfactualVariable   
+
+__all__ = [
+    'Interval',
+    'Fluent'
+]
 class Interval(Element):
     """Timing and ordering queries.  
     
@@ -99,19 +104,33 @@ class Interval(Element):
         return (self.start == other.start) and (self.end == other.end)
 
 class Fluent(Element):
+    """Any pattern that persists over time is called a fluent.  
+    
+    For example, the pattern of interest might be that the size of the cohort of sick people is greater than 10% of the population.  
+    If this persists over time, it is a fluent.   
+    In logical approaches to reasoning about actions, fluents can be represented in first-order logic by predicates having an argument that depends on time. 
+    For example, the condition "the box is on the table", if it can change over time, cannot be represented by 
+    {\displaystyle \mathrm {On} (\mathrm {box} ,\mathrm {table} )}{\displaystyle \mathrm {On} (\mathrm {box} ,\mathrm {table} )}; 
+    a third argument is necessary to the predicate {\displaystyle \mathrm {On} }{\displaystyle \mathrm {On} } to specify the time: 
+    {\displaystyle \mathrm {On} (\mathrm {box} ,\mathrm {table} ,t)}{\displaystyle \mathrm {On} (\mathrm {box} ,\mathrm {table} ,t)} 
+    means that the box is on the table at time {\displaystyle t}t. 
+    This representation of fluents is modified in the situation calculus by using the sequence of the past actions in place of the current time.
+
+    https://en.wikipedia.org/wiki/Fluent_(artificial_intelligence)
+    """
     def __init__(self, name, interval: Interval):
         self.name == name
         self.interval == interval
 
     def to_text(self):
         """Output this fluent in the internal string format."""
-        return f"{self.name}[{self.interval}]"
+        return f"{self.name}[{self.interval.to_latex()}]"
     
     def to_latex(self):
         """Output this fluent in the LaTeX string format.
 
         :returns: The LaTeX representation of this fluent.
         """
-        return f"\texttt{{{self.name}}}\left({self.interval}\right)"
+        return rf"\texttt{{{self.name}}}\left({self.interval.to_latex()}\right)"
     
     
---
+++ tests/test_dedri.py
@@ -0,0 +1,48 @@
+import unittest
+from y0.dedri import (
+    Fluent,
+    Interval,
+
+)
+
+class TestDeDri(unittest.TestCase):
+    """Tests for the stringifying instances of the probability DSL."""
+    def test_interval(self):
+        """Test that the 13 allen interval relations work as expected"""
+        a = Interval('a', 1, 10)
+        b = Interval('b', 11, 20)
+        c = Interval('c', 5, 15)
+        d = Interval('d', 10, 11)
+        e = Interval('e', 1, 20)
+        f = Interval('f', 1, 10)
+
+        self.assertTrue(a < b)
+        self.assertFalse(a > b)
+        self.assertTrue( b > a)
+        self.assertFalse( b < a)
+        self.assertTrue(a.meets(d))
+        self.assertFalse(d.meets(a))
+        self.assertTrue(a.is_met_by(d))
+        self.assertFalse(d.is_met_by(d))
+        self.assertTrue(a.overlaps_with(b))
+        self.assertFalse(b.overlaps_with(b))
+        self.assertTrue(b.is_overlapped_by(a))
+        self.assertFalse(a.is_overlapped_by(b))
+        self.assertTrue(a.starts(e))
+        self.assertFalse(e.starts(a))
+        self.assertTrue(e.is_started_by(a))
+        self.assertFalse(a.is_started_by(e))
+        self.assertTrue(c.during(e))
+        self.assertFalse(e.during(c))
+        self.assertTrue(e.contains(c))
+        self.assertFalse(c.contains(e))
+        self.assertTrue(b.finishes(e))
+        self.assertFalse(e.finishes(b))
+        self.assertTrue(e.is_finished_by(b))
+        self.assertFalse(b.is_finished_by(e))
+        self.assertTrue(a == f)
+        self.assertFalse(a == b)
+        self.assertFalse( a == c)
+        self.assertFalse( a == d)
+        self.assertFalse(a == e)
+        
