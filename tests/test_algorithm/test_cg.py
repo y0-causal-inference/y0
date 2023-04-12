69c1414a25215e57b53ddac1092145467f56d3c1
Jeremy Zucker <Jeremy.Zucker@pnnl.gov>
2023-04-12 19:49:00 -0400

    Converging on test_counterfactual_graph through intermediate graphs in 11a, 11b, and 11c

--- tests/test_algorithm/test_cg.py
+++ tests/test_algorithm/test_cg.py
@@ -17,9 +17,10 @@ from y0.algorithm.identify.cg import (
     stitch_counterfactual_and_neighbors,
     stitch_factual_and_doppleganger_neighbors,
     stitch_factual_and_dopplegangers,
+    make_counterfactual_graph
 )
 from y0.dsl import D, W, X, Y, Z
-from y0.examples import figure_9a, figure_9b, figure_11a, figure_11b, figure_11c
+from y0.examples import figure_9a, figure_9b, figure_9c, figure_9d, figure_11a, figure_11b, figure_11c
 from y0.graph import NxMixedGraph
 
 x, y, z, d, w = -X, -Y, -Z, -D, -W
@@ -41,13 +42,15 @@ class TestCounterfactualGraph(cases.GraphTestCase):
         self.assert_graph_equal(expected, actual)
 
     def test_has_same_function(self):
-        """Test that two variables have the same function."""
+        """Test that two variables have the same value."""
         self.assertTrue(has_same_function(D @ X, D))
         self.assertTrue(has_same_function(D @ D, D))
+        self.assertFalse(has_same_function(X, X @ +x))
         self.assertTrue(has_same_function(X @ D, X))
         self.assertFalse(has_same_function(X, D))
         self.assertFalse(has_same_function(X @ ~X, W @ ~X))
-        self.assertTrue(has_same_function(X @ ~X, X))
+        self.assertFalse(has_same_function(X @ ~X, X))
+        self.assertTrue(has_same_function(D, D @ ~x))
 
     def test_has_same_parents(self):
         """Test that all parents of two nodes are the same."""
@@ -57,6 +60,9 @@ class TestCounterfactualGraph(cases.GraphTestCase):
         self.assertFalse(has_same_parents(figure_9b.graph, X, D))
         self.assertFalse(has_same_parents(figure_9b.graph, X @ ~X, W @ ~X))
         self.assertFalse(has_same_parents(figure_9b.graph, X @ ~X, X))
+        self.assertFalse(has_same_parents(figure_9b.graph, Z, Z @ ~x))
+        self.assertTrue(has_same_parents(figure_11a.graph, Z, Z @ ~x))
+
 
     def test_get_worlds(self):
         """Test that all interventions within each world of a counterfactual conjunction are generated."""
@@ -368,6 +374,12 @@ class TestCounterfactualGraph(cases.GraphTestCase):
         self.assertTrue(is_pw_equivalent(figure_9b.graph, X @ D, X))
         self.assertFalse(is_pw_equivalent(figure_9b.graph, Z @ D, Z))
         self.assertFalse(is_pw_equivalent(figure_9b.graph, X, X @ ~X))
+        self.assertFalse(is_pw_equivalent(figure_9b.graph, Z, Z @ ~X))
+        self.assertTrue(is_pw_equivalent(figure_11a.graph, Z, Z @ ~X))
+        self.assertFalse(is_pw_equivalent(figure_9b.graph, D, D @ -d))
+        self.assertTrue(is_pw_equivalent(figure_11a.graph, D @ -d, D))
+        self.assertFalse(is_pw_equivalent(figure_11a.graph, X @ +x, X))
+
 
     def test_merge_pw(self):
         """Test that the parallel worlds graph after merging two nodes is correct.
@@ -391,42 +403,42 @@ class TestCounterfactualGraph(cases.GraphTestCase):
         self.assert_graph_equal(merge_pw(figure_9b.graph, Z, W), cf_graph_9)
 
 
-    # def test_make_counterfactual_graph(self):
-    #     r"""Test making a counterfactual graph.
+    def test_make_counterfactual_graph(self):
+        r"""Test making a counterfactual graph.
 
-    #     The invocation of **make-cg** with the graph in Figure 9(a) and the joint distribution
-    #     :math:`P(y_x, x', z, d)` will result in the counterfactual graph shown in Fig. 9(c).
-    #     The invocation of **make-cg** with the graph in Figure 9(a) and the joint distribution
-    #     :math:`P(y_{x,z},x')` will result in the counterfactual graph shown in Fig. 9(d).
-    #     """
-    #     actual_graph, actual_event = make_counterfactual_graph(
-    #         figure_9a.graph, {Y @ +x: +y, X: -x, Z @ -d: -z, D: -d}
-    #     )
-    #     self.assert_graph_equal(figure_9c.graph, actual_graph)
-    #     self.assertEqual({Y @ +x: +y, X: -x, Z: -z, D: -d}, actual_event)
-    #     actual_graph2, actual_event2 = make_counterfactual_graph(
-    #         figure_9a.graph, {Y @ (+x, -z): +y, X: -x}
-    #     )
-    #     expected_graph2, expected_event2 = figure_9d.graph, {Y @ (+x, -z): +y, X: -x}
-    #     self.assertEqual(expected_event2, actual_event2)
-    #     self.assert_graph_equal(expected_graph2, actual_graph2)
+        The invocation of **make-cg** with the graph in Figure 9(a) and the joint distribution
+        :math:`P(y_x, x', z, d)` will result in the counterfactual graph shown in Fig. 9(c).
+        The invocation of **make-cg** with the graph in Figure 9(a) and the joint distribution
+        :math:`P(y_{x,z},x')` will result in the counterfactual graph shown in Fig. 9(d).
+        """
+        actual_graph, actual_event = make_counterfactual_graph(
+            figure_9a.graph, {Y @ +x: +y, X: -x, Z @ -d: -z, D: -d}
+        )
+        self.assert_graph_equal(figure_9c.graph, actual_graph)
+        self.assertEqual({Y @ +x: +y, X: -x, Z: -z, D: -d}, actual_event)
+        actual_graph2, actual_event2 = make_counterfactual_graph(
+            figure_9a.graph, {Y @ (+x, -z): +y, X: -x}
+        )
+        expected_graph2, expected_event2 = figure_9d.graph, {Y @ (+x, -z): +y, X: -x}
+        self.assertEqual(expected_event2, actual_event2)
+        self.assert_graph_equal(expected_graph2, actual_graph2)
 
-    #     # Check for inconsistent counterfactual values for merged nodes
-    #     actual_graph3, actual_event3 = make_counterfactual_graph(
-    #         graph=NxMixedGraph.from_edges(directed=[(D, Z), (Z, Y)]),
-    #         event={Z @ -d: -z, Z: +z, D: -d},
-    #     )
-    #     self.assertIsNone(actual_event3)
+        # Check for inconsistent counterfactual values for merged nodes
+        actual_graph3, actual_event3 = make_counterfactual_graph(
+            graph=NxMixedGraph.from_edges(directed=[(D, Z), (Z, Y)]),
+            event={Z @ -d: -z, Z: +z, D: -d},
+        )
+        self.assertIsNone(actual_event3)
 
-    #     # Check whether {Y_{+x,z,w): -y, X_w: -x} automatically simplifies to {Y_{z,w}: y, X: -x} (it should not)
-    #     actual_graph4, actual_event4 = make_counterfactual_graph(
-    #         graph=figure_9a.graph, event={X @ -W: X, Y @ (-W, +X, -Z): Y}
-    #     )
-    #     expected_event4 = {Y @ (-W, -Z): Y, X: X}
-    #     expected_graph4 = NxMixedGraph.from_edges(
-    #         nodes={W @ -W, Y @ (-W, +X, -Z), Z @ (-W, +X, -Z), X},
-    #         directed={(W @ -W, Y @ (-W, +X, -Z)), (Z @ (-W, +X, -Z), Y @ (-W, +X, -Z))},
-    #         undirected={frozenset({X, Y @ (-W, +X, -Z)})},
-    #     )
-    #     self.assertNotEqual(expected_event4, actual_event4)
-    #     self.assert_graph_equal(expected_graph4, actual_graph4)
+        # Check whether {Y_{+x,z,w): -y, X_w: -x} automatically simplifies to {Y_{z,w}: y, X: -x} (it should not)
+        actual_graph4, actual_event4 = make_counterfactual_graph(
+            graph=figure_9a.graph, event={X @ -W: X, Y @ (-W, +X, -Z): Y}
+        )
+        expected_event4 = {Y @ (-W, -Z): Y, X: X}
+        expected_graph4 = NxMixedGraph.from_edges(
+            nodes={W @ -W, Y @ (-W, +X, -Z), Z @ (-W, +X, -Z), X},
+            directed={(W @ -W, Y @ (-W, +X, -Z)), (Z @ (-W, +X, -Z), Y @ (-W, +X, -Z))},
+            undirected={frozenset({X, Y @ (-W, +X, -Z)})},
+        )
+        self.assertNotEqual(expected_event4, actual_event4)
+        self.assert_graph_equal(expected_graph4, actual_graph4)
