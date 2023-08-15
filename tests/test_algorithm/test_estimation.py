784e7b1ffadff24a0f9ba45a7a13fe4744b08431
Sara Taheri <srtaheri66@gmail.com>
2023-08-15 12:18:37 -0400

    add tests for test_is_a_fixable

--- tests/test_algorithm/test_estimation.py
+++ tests/test_algorithm/test_estimation.py
@@ -103,13 +103,77 @@ class TestEstimation(unittest.TestCase):
     def test_is_a_fixable(self):
         """Test checking for a-fixability."""
         # see examples at https://gitlab.com/causal/ananke/-/blob/dev/tests/estimation/test_counterfactual_mean.py?ref_type=heads#L20-47
-        graph_1 = ...
-        treatment_1 = ...
-        self.assertFalse(is_a_fixable(graph_1, treatment_1))
+        graph_1 = NxMixedGraph.from_str_edges(
+            directed=[("T", "M"), ("M", "L"), ("L", "Y")], undirected=[("M", "Y")]
+        )
+        treatment_1 = "T"
+        self.assertTrue(is_a_fixable(graph_1, treatment_1))
 
-        graph_2 = ...
-        treatment_2 = ...
-        self.assertTrue(is_a_fixable(graph_2, treatment_2))
+        graph_2 = napkin.graph
+        treatment_2 = "X"
+        self.assertFalse(is_a_fixable(graph_2, treatment_2))
+
+        graph_3 = NxMixedGraph.from_str_edges(
+            directed=[
+                ("ADAM17", "EGFR"),
+                ("ADAM17", "TNF"),
+                ("ADAM17", "Sil6r"),
+                ("EGFR", "cytok"),
+                ("TNF", "cytok"),
+                ("Sil6r", "IL6STAT3"),
+                ("IL6STAT3", "cytok"),
+            ],
+            undirected=[
+                ("ADAM17", "cytok"),
+                ("ADAM17", "Sil6r"),
+                ("EGFR", "TNF"),
+                ("EGFR", "IL6STAT3"),
+            ],
+        )
+        treatment_3 = "EGFR"
+        self.assertTrue(is_a_fixable(graph_3, treatment_3))
+
+        graph_4 = NxMixedGraph.from_str_edges(
+            directed=[
+                ("SOS", "Ras"),
+                ("Ras", "PI3K"),
+                ("Ras", "Raf"),
+                ("PI3K", "AKT"),
+                ("AKT", "Raf"),
+            ],
+            undirected=[("SOS", "PI3K")],
+        )
+        treatment_4 = "PI3K"
+        self.assertTrue(is_a_fixable(graph_4, treatment_4))
+
+        graph_5 = NxMixedGraph.from_str_edges(
+            directed=[
+                ("Z1", "X"),
+                ("X", "M1"),
+                ("M1", "Y"),
+                ("Z1", "Z2"),
+                ("Z2", "Z3"),
+                ("Z3", "Y"),
+                ("Z2", "M1"),
+            ],
+            undirected=[("Z1", "X"), ("Z2", "M1")],
+        )
+        treatment_5 = "X"
+        self.assertTrue(is_a_fixable(graph_5, treatment_5))
+
+        graph_6 = NxMixedGraph.from_str_edges(
+            directed=[("Z1", "X"), ("X", "M1"), ("M1", "Y"), ("Z1", "Y")], undirected=[]
+        )
+        treatment_6 = "X"
+        self.assertTrue(is_a_fixable(graph_6, treatment_6))
+
+        graph_7 = NxMixedGraph.from_str_edges(directed=[("A", "B"), ("B", "C")], undirected=[])
+        treatment_7 = "A"
+        self.assertTrue(is_a_fixable(graph_7, treatment_7))
+
+        graph_8 = graph_4
+        treatment_8 = "SOS"
+        self.assertFalse(is_a_fixable(graph_8, treatment_8))
 
     def test_is_p_fixable(self):
         """Test checking for p-fixability."""
