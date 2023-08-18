2fea591a6f2b5a2c2e2447303a546fa5841ccdf7
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-08-18 12:50:19 +0200

    Update constants.py

--- tests/constants.py
+++ tests/constants.py
@@ -4,4 +4,4 @@ __all__ = ["HERE", "DATA_DIRECTORY", "NAPKIN_TEST_PATH"]
 
 HERE = Path(__file__).parent.resolve()
 DATA_DIRECTORY = HERE.joinpath("data")
-NAPKIN_TEST_PATH = DATA_DIRECTORY.joinpath("napkin_test.tsv")
+NAPKIN_TEST_PATH = DATA_DIRECTORY.joinpath("napkin", "observational.tsv")
