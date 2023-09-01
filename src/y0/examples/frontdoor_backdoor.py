2784c6d7d3bcb04ca4975216c5c1288cf6958f84
Sara Taheri <srtaheri66@gmail.com>
2023-09-01 10:31:56 -0400

    changed data geberation to generate only binary data

--- src/y0/examples/frontdoor_backdoor.py
+++ src/y0/examples/frontdoor_backdoor.py
@@ -1,27 +1,26 @@
 import numpy as np
 import pandas as pd
 
-from y0.dsl import Variable, X, Y, Z, W
+from y0.dsl import Variable, W, X, Y, Z
 
 __all__ = [
-    "generate_data_for_frontdoor",
+    "generate_data_for_frontdoor_backdoor",
 ]
 
 
-def _r_exp(x):
-    return 1 / (1 + np.exp(x))
-
-
-def generate_data_for_frontdoor(
-    num_samples: int, treatments: dict[Variable, float] | None = None, *, seed: int | None = None
+def generate_data_for_frontdoor_backdoor(
+    num_samples: int,
+    treatments: dict[Variable, float] | None = None,
+    *,
+    seed: int | None = None,
 ) -> pd.DataFrame:
-    """Generate testing data for the front-door case study.
+    """Generate testing data for the front-door back_door case study.
 
     :param num_samples: The number of samples to generate. Try 1000.
     :param treatments: An optional dictionary of the values to fix each variable to.
     :param seed: An optional random seed for reproducibility purposes
     :returns: A pandas Dataframe with columns corresponding
-        to the variable names in the front-door example
+        to the variable names in the front-door back_door example
     """
     if treatments is None:
         treatments = {}
@@ -29,35 +28,31 @@ def generate_data_for_frontdoor(
     if W in treatments:
         w = np.full(num_samples, treatments[W])
     else:
-        w = generator.normal(loc=40.0, scale=10.0, size=num_samples)
+        w = generator.binomial(n=1, p=0.3, size=num_samples)
 
-    beta0_x = -1
-    beta_w_to_x = 0.05
+    beta0_x = 0.1
+    beta_w_to_x = 0.5
     if X in treatments:
         x = np.full(num_samples, treatments[X])
     else:
-        p = _r_exp(-beta0_x - w * beta_w_to_x)
-        x = generator.binomial(1, p, size=num_samples)
+        p_x = beta0_x + w * beta_w_to_x
+        x = generator.binomial(n=1, p=p_x, size=num_samples)
 
-    beta0_z = -1.9
-    beta_x_to_z = 0.04
+    beta0_z = 0.5
+    beta_x_to_z = 0.4
     if Z in treatments:
         z = np.full(num_samples, treatments[Z])
     else:
-        z = generator.normal(
-            loc=100 * _r_exp(-beta0_z - x * beta_x_to_z), scale=1, size=num_samples
-        )
+        p_z = beta0_z + x * beta_x_to_z
+        z = generator.binomial(n=1, p=p_z, size=num_samples)
 
-    beta0_y = -1.8
-    beta_z_to_y = 0.05
-    beta_w_to_y = 0.06
+    beta0_y = 0.1
+    beta_z_to_y = 0.7
+    beta_w_to_y = 0.1
     if Y in treatments:
         y = np.full(num_samples, treatments[Y])
     else:
-        y = generator.normal(
-            loc=100 * _r_exp(-beta0_y - z * beta_z_to_y - w * beta_w_to_y),
-            scale=1,
-            size=num_samples,
-        )
+        p_y = beta0_y + z * beta_z_to_y + w * beta_w_to_y
+        y = generator.binomial(n=1, p=p_y, size=num_samples)
 
     return pd.DataFrame({X.name: x, Z.name: z, W.name: w, Y.name: y})
