b6396dcd41acf9c5833e0b6d39cba495c2f7cec8
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-09-25 15:00:22 +0200

    Make pgmpy estimators gettable

--- src/y0/struct.py
+++ src/y0/struct.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from functools import lru_cache
 from typing import Iterable, NamedTuple, Optional, Tuple, Union
 
 import pandas as pd
@@ -30,7 +31,7 @@ class VermaConstraint(NamedTuple):
     def from_element(cls, element) -> VermaConstraint:
         """Extract content from each element in the vector returned by `verma.constraint`.
 
-        :param element: An element in the in the vector returned by `verma.constraint`
+        :param element: An element in the vector returned by `verma.constraint`
         :returns: A Verma constraint tuple for the given element
 
         .. seealso:: Extracting from R objects https://rpy2.github.io/doc/v3.4.x/html/vector.html#extracting-items
@@ -45,6 +46,27 @@ class VermaConstraint(NamedTuple):
             lhs_expr=parse_causaleffect(_extract(element, "lhs.expr")),
             variables=_parse_vars(element),
         )
+
+
+@lru_cache
+def get_conditional_independence_tests():
+    """Get the conditional independence tests from :mod:`pgmpy.estimators.CITests`."""
+    try:
+        from pgmpy.estimators import CITests
+    except ImportError as e:
+        raise ImportError("Calculating falsifications requires `pip install pgmpy`.") from e
+
+    return {
+        "pearson": CITests.pearsonr,
+        "chi-square": CITests.chi_square,
+        "cressie_read": CITests.cressie_read,
+        "freeman_tuckey": CITests.freeman_tuckey,
+        "g_sq": CITests.g_sq,
+        "log_likelihood": CITests.log_likelihood,
+        "modified_log_likelihood": CITests.modified_log_likelihood,
+        "power_divergence": CITests.power_divergence,
+        "neyman": CITests.neyman,
+    }
 
 
 @dataclass(frozen=True)
@@ -105,25 +127,7 @@ class DSeparationJudgement:
             value, then returns simply a boolean if the test fails.
         :raises ValueError: if any parts of the judgement aren't in the dataframe's
             columns
-        :raises ImportError: If pgmpy is not available. Install with pip.
         """
-        try:
-            from pgmpy.estimators import CITests
-        except ImportError as e:
-            raise ImportError("Calculating falsifications requires `pip install pgmpy`.") from e
-
-        tests = {
-            "pearson": CITests.pearsonr,
-            "chi-square": CITests.chi_square,
-            "cressie_read": CITests.cressie_read,
-            "freeman_tuckey": CITests.freeman_tuckey,
-            "g_sq": CITests.g_sq,
-            "log_likelihood": CITests.log_likelihood,
-            "modified_log_likelihood": CITests.modified_log_likelihood,
-            "power_divergence": CITests.power_divergence,
-            "neyman": CITests.neyman,
-        }
-
         if self.left.name not in df.columns:
             raise ValueError(
                 f"left variable {self.left.name} ({type(self.left.name)}) not in columns {list(df.columns)}"
@@ -138,6 +142,7 @@ class DSeparationJudgement:
                     f"conditional {c.name} ({type(c.name)}) not in columns {df.columns}"
                 )
 
+        tests = get_conditional_independence_tests()
         func = tests[method or "cressie_read"]
         return func(
             X=self.left.name,
