a15a4fbce26bb7fb91406efab4fb19b89b6fa885
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-10-28 11:32:38 +0200

    Update simplify_latent.py

--- src/y0/algorithm/simplify_latent.py
+++ src/y0/algorithm/simplify_latent.py
@@ -3,7 +3,6 @@
 """Implement Robin Evans' simplification algorithms.
 
 .. seealso:: https://www.fields.utoronto.ca/programs/scientific/11-12/graphicmodels/Evans.pdf slides 34-43
-
 """
 
 import itertools as itt
