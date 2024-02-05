5ba72a04e41ea54942147a02e068b11cef379790
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-02-05 18:10:00 +0100

    Add first step for generating a linear SCM

--- src/y0/algorithm/estimation/linear_scm.py
+++ src/y0/algorithm/estimation/linear_scm.py
@@ -7,6 +7,8 @@ from sklearn.linear_model import LinearRegression
 
 from y0.dsl import Variable
 from y0.graph import NxMixedGraph
+import networkx as nx
+import sympy as sy
 
 __all__ = [
     "get_single_door",
@@ -35,6 +37,51 @@ def get_single_door(
             coefficients.append(model.coef_[idx])
         rv[source, target] = fmean(coefficients)
     return rv
+
+
+def evaluate_admg(graph, data: pd.DataFrame):
+    params = {_get_beta(l, r): v for (l, r), v in get_single_door(graph, data).items()}
+    # do something
+    lscm = generate_lscm_from_mixed_graph(graph)
+    return evaluate_lscm(lscm, params)
+
+
+def evaluate_lscm(
+    LSCM: dict[sy.Symbol, sy.Expr], params: dict[sy.Symbol, float]
+) -> dict[sy.Symbol, sy.core.numbers.Rational]:
+    """given an LSCM, assign values to the parameters (i.e. beta, epsilon, gamma terms), and return variable assignments dictionary"""
+    # solve set of simulateous linear equations in sympy
+    eqns = [sy.Eq(lhs.subs(params), rhs.subs(params)) for lhs, rhs in LSCM.items()]
+    print(eqns)
+    return sy.solve(eqns, list(LSCM))
+
+
+def _get_beta(left: Variable, right: Variable) -> sy.Symbol:
+    return sy.Symbol(f"beta_{left.name}_->{right.name}")
+
+
+def generate_lscm_from_mixed_graph(graph: NxMixedGraph) -> dict:
+    equations = {}
+    for node in graph.topological_sort():
+        node_sym = sy.Symbol(node.name)  # fix name prop
+        expression_terms = []
+
+        # Add parent edges
+        for parent in graph.directed.predecessors(node):
+            expression_terms.append(_get_beta(parent, node) * sy.Symbol(f"{parent.name}"))
+
+        # Add noise term
+        epsilon_sym = sy.Symbol(f"epsilon_{node.name}")
+        expression_terms.append(epsilon_sym)
+
+        # get bidirected edges
+        for u, v in graph.undirected.edges(node):
+            u, v = sorted([u, v])
+            temp_gamma_sym = sy.Symbol(f"gamma_{u}_<->{v}")
+            expression_terms.append(temp_gamma_sym)
+
+        equations[node_sym] = sum(expression_terms)
+    return equations
 
 
 def _main():
