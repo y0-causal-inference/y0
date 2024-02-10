7f99cd5c704a9cabd3abac1e7d2a5641fbabcd0c
Jeremy Zucker <Jeremy.Zucker@pnnl.gov>
2024-02-09 21:26:34 -0500

    Added front door, backdor and napkin tests for single door criterion.  Tests pass

--- tests/test_algorithm/test_linear_scm.py
+++ tests/test_algorithm/test_linear_scm.py
@@ -7,8 +7,8 @@ import unittest
 import pandas as pd
 
 from y0.algorithm.estimation.linear_scm import get_single_door
-from y0.dsl import X, Y, Z
-from y0.examples import backdoor_example, frontdoor_example
+from y0.dsl import Z1, Z2, X, Y, Z
+from y0.examples import backdoor_example, frontdoor_example, napkin_example
 from y0.graph import NxMixedGraph
 
 
@@ -17,12 +17,13 @@ class TestLinearSCM(unittest.TestCase):
 
     def setUp(self):
         """Set up the test case."""
-        self.backdoor_example = backdoor_example
         self.backdoor_df = pd.read_csv("tests/test_algorithm/backdoor_example.csv")
+        self.frontdoor_df = pd.read_csv("tests/test_algorithm/frontdoor_example.csv")
+        self.napkin_df = pd.read_csv("tests/test_algorithm/napkin_example.csv")
 
-    def test_get_single_door(self):
-        """Test that the single door criterion works as expected."""
-        actual_backdoor_parameters = get_single_door(self.backdoor_example.graph, self.backdoor_df)
+    def test_get_single_door_backdoor(self):
+        """Test that the single door criterion works as expected on the backdoor example."""
+        actual_backdoor_parameters = get_single_door(backdoor_example.graph, self.backdoor_df)
         expected_backdoor_parameters = {
             (Z, X): 0.4659813040231536,
             (Z, Y): 0.5331826121659358,
@@ -30,3 +31,17 @@ class TestLinearSCM(unittest.TestCase):
         }
         for key, actual_value in actual_backdoor_parameters.items():
             self.assertAlmostEqual(expected_backdoor_parameters[key], actual_value)
+
+    def test_get_single_door_frontdoor(self):
+        """Test that the single door criterion works as expected on the frontdoor example."""
+        actual_frontdoor_parameters = get_single_door(frontdoor_example.graph, self.frontdoor_df)
+        expected_frontdoor_parameters = {(X, Z): 0.8074475606195012, (Z, Y): 0.30077413265953384}
+        for key, actual_value in actual_frontdoor_parameters.items():
+            self.assertAlmostEqual(expected_frontdoor_parameters[key], actual_value)
+
+    def test_get_single_door_napkin(self):
+        """Test that the single door criterion works as expected on the napkin example."""
+        actual_napkin_parameters = get_single_door(napkin_example.graph, self.napkin_df)
+        expected_napkin_parameters = {(Z2, Z1): 0.44658822516963126, (Z1, X): 0.3832318708801637}
+        for key, actual_value in actual_napkin_parameters.items():
+            self.assertAlmostEqual(expected_napkin_parameters[key], actual_value)
