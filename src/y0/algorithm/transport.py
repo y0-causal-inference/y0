86a1559102c5a0cf09ef5ce49221214026c74b26
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-08-19 19:01:46 +0200

    Update transport.py

--- src/y0/algorithm/transport.py
+++ src/y0/algorithm/transport.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Implement of surrogate outcomes and transportability from https://arxiv.org/abs/1806.07172.
-
-.. todo::
-
-    Add high level documentation to "transport" module
-
-    1. What problem are we trying to solve here?
-    2. What's the difference between surrogate outcomes and transportability?
-    3. Real world example
-"""
+"""Implement of surrogate outcomes and transportability from https://arxiv.org/abs/1806.07172."""
 
 import logging
 from copy import deepcopy
