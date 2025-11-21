98f958fd9d5bc31fff1804507f16add3ae35fd28
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-21 09:19:12 +0100

    Additional test consolidation

--- tests/test_algorithm/test_ioscm_id.py
+++ tests/test_algorithm/test_ioscm_id.py
@@ -7,6 +7,7 @@
 
 import unittest
 
+from tests.test_algorithm import cases
 from y0.algorithm.ioscm_id import (
     _simplify_strongly_connected_components,
     get_apt_order,
@@ -43,37 +44,27 @@ simple_cyclic_graph_2 = NxMixedGraph.from_edges(
 )
 
 
-class TestConvertStronglyConnectedComponents(unittest.TestCase):
-    """Tests converting strongly connected components in a graph to bidirected edges."""
+class TestIOSCMUtils(cases.GraphTestCase):
+    """Test ioSCMs utilities."""
 
-    def test_convert_strongly_connected_components_1(self) -> None:
-        """First test converting strongly connected components in a graph to bidirected edges."""
-        result_1 = scc_to_bidirected(simple_cyclic_graph_1)
-        self.assertSetEqual(set(result_1.undirected.edges), {(X, W), (W, Z), (X, Z)})
-        self.assertSetEqual(set(result_1.directed.edges), {(R, X), (W, Y)})
+    def test_convert_strongly_connected_components(self) -> None:
+        """Test converting strongly connected components in a graph to bidirected edges."""
+        for graph, undirected, directed in [
+            (simple_cyclic_graph_1, {(X, W), (W, Z), (X, Z)}, {(R, X), (W, Y)}),
+            (simple_cyclic_graph_2, {(X, R), (X, W), (W, Z), (X, Z)}, {(W, Y)}),
+        ]:
+            bidirected_graph = scc_to_bidirected(graph)
+            self.assertSetEqual(undirected, set(bidirected_graph.undirected.edges))
+            self.assertSetEqual(set(bidirected_graph.directed.edges), directed)
 
-    def test_convert_strongly_connected_components_2(self) -> None:
-        """Second test converting strongly connected components in a graph to bidirected edges."""
-        result_2 = scc_to_bidirected(simple_cyclic_graph_2)
-        self.assertSetEqual(set(result_2.undirected.edges), {(X, R), (X, W), (W, Z), (X, Z)})
-        self.assertSetEqual(set(result_2.directed.edges), {(W, Y)})
-
-
-class TestGetConsolidatedDistrict(unittest.TestCase):
-    """Test retrieving a consolidated district in a graph with or without cycles."""
-
-    # TODO: Implement type checking on the graph and the input variable.
-    # TODO: Also check that we can't pass in multiple vertices or an empty graph.
-    # TODO: Add more tests.
-
-    def test_get_vertex_consolidated_district_1(self) -> None:
-        """First test for getting the consolidated district for a single vertex."""
+    def test_get_vertex_consolidated_district(self) -> None:
+        """Test for getting the consolidated district for a single vertex."""
         for vertex, result in [(X, {X, W, Z, R}), (R, {X, W, Z, R}), (Y, {Y})]:
             self.assertSetEqual(
                 result, get_vertex_consolidated_district(simple_cyclic_graph_2, vertex)
             )
 
-    def test_get_consolidated_district_1(self) -> None:
+    def test_get_consolidated_district(self) -> None:
         """Test getting consolidated districts."""
         for vertices, result in [
             # Single vertex input
@@ -87,7 +78,7 @@ class TestGetConsolidatedDistrict(unittest.TestCase):
         ]:
             self.assertSetEqual(get_consolidated_district(simple_cyclic_graph_2, vertices), result)
 
-    def test_get_graph_consolidated_district_1(self) -> None:
+    def test_get_graph_consolidated_district(self) -> None:
         """First test for getting the consolidated districts for a graph."""
         # create a graph where multiple nodes belong to the same consolidated district
         # create a graph with bidirected edges forming one district
@@ -102,40 +93,18 @@ class TestGetConsolidatedDistrict(unittest.TestCase):
         ]:
             self.assertSetEqual(result, get_graph_consolidated_districts(graph))
 
-
-class TestAptOrder(unittest.TestCase):
-    """Test retrieving an apt-order for a graph G."""
-
-    # TODO: Implement type checking on the graph and the input variable.
-    # TODO: Also check that we can't pass in multiple vertices or an empty graph.
-    # TODO: Add more tests.
-
-    def test_simplify_strongly_connected_components_1(self) -> None:
-        """Test a utility function to simplify strongly-connected components for a graph."""
-        # TODO: Check the returned dictionaries.
-        result_1, _result_1_dict = _simplify_strongly_connected_components(simple_cyclic_graph_1)
-        # Make the representative vertex W
-        expected_result = NxMixedGraph.from_edges(directed=[(R, W), (W, Y)])
-        self.assertListEqual(
-            sorted(result_1.directed.edges), sorted(expected_result.directed.edges)
-        )
-        self.assertListEqual(
-            sorted(result_1.undirected.edges), sorted(expected_result.undirected.edges)
-        )
-
-    def test_simplify_strongly_connected_components_2(self) -> None:
-        """Test a utility function to simplify strongly-connected components for a graph."""
-        # TODO: Check the returned dictionaries.
-        result_2, _result_2_dict = _simplify_strongly_connected_components(simple_cyclic_graph_2)
-        # Make the representative vertex W
-        expected_result_2 = NxMixedGraph.from_edges(directed=[(W, Y)], undirected=[(R, W)])
-        self.assertListEqual(
-            sorted(expected_result_2.directed.edges), sorted(result_2.directed.edges)
-        )
-        self.assertListEqual(
-            sorted(expected_result_2.undirected.edges),
-            sorted(result_2.undirected.edges),
-        )
+    def test_simplify_strongly_connected_components(self) -> None:
+        """Test simplify strongly-connected components for a graph."""
+        for graph, expected in [
+            (simple_cyclic_graph_1, NxMixedGraph.from_edges(directed=[(R, W), (W, Y)])),
+            (
+                simple_cyclic_graph_2,
+                NxMixedGraph.from_edges(directed=[(W, Y)], undirected=[(R, W)]),
+            ),
+        ]:
+            # TODO test result dict
+            actual, _result_dict = _simplify_strongly_connected_components(graph)
+            self.assert_graph_equal(expected, actual)
 
     def test_simplify_strongly_connected_components_3(self) -> None:
         """Test a utility function to simplify strongly-connected components for a graph.
@@ -169,18 +138,13 @@ class TestAptOrder(unittest.TestCase):
         # (the X <-> W edge should be removed via the continue statement)
         self.assertEqual(len(list(simplified_graph.undirected.edges)), 0)
 
-        # FIXME there's a logical error in the tests, just above it's checking
-        #  that there are 2 nodes. Check this.
-        # checking that the result has only one node (the representattive to Y)
-        # self.assertEqual(len(simplified_graph.nodes()), 1)
-
         # verify the representative node maps to all three nodes in the component
         representative = min([X, W, Z])  # W is the min = representative
         self.assertIn(representative, result_dict)
         self.assertEqual(result_dict[representative], frozenset({X, W, Z}))
 
-    def test_get_apt_order_1(self) -> None:
-        """First test for getting an assembling pseudo-topological order for a graph."""
+    def test_get_apt_order(self) -> None:
+        """Test getting an assembling pseudo-topological order for a graph."""
         for graph, result in [
             (simple_cyclic_graph_1, [R, W, X, Z, Y]),
             # For this second test, R is allowed to occur before W, X, Z as well, but it comes after
@@ -191,7 +155,7 @@ class TestAptOrder(unittest.TestCase):
 
     @unittest.skip(reason="not implemented")
     def test_is_apt_order_1(self) -> None:
-        """First test for verifying an assembling pseudo-topological order for a graph."""
+        """Test verifying an assembling pseudo-topological order for a graph."""
         for order in [
             [R, X, W, Z, Y],
             [R, X, Z, W, Y],
