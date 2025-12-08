010e1a18f06949e92e67b3569aa62d7eca7893d4
Dogbatse <emily.dogbatse@pnnl.gov>
2025-12-08 13:26:16 -0600

    working on idcd helper function

---
+++ src/y0/algorithm/identify/idcd.py
@@ -0,0 +1,9 @@
+"""
+IDCD Algorith Implementation (Algorith 1, Lines 13-28) from Forré & Mooij (2019).
+
+This module implements the IDCD (IDentification within Consolidated Districts) algorithm,
+which is a helper function for the main cyclic ID algorithm. IDCD handles identification
+of causal effects within consolidated districts of directed mixed graphs that may contain
+cycles and latent confounders.
+
+"""
