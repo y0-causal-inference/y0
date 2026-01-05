8e28574949e9614b119479688698f14906c41643
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-01-05 11:35:02 +0100

    Update pyparsing warnings

--- src/y0/parser/ce/utils.py
+++ src/y0/parser/ce/utils.py
@@ -8,7 +8,7 @@ from pyparsing import (
     Word,
     alphanums,
     alphas,
-    delimitedList,
+    DelimitedList,
     nums,
 )
 
@@ -24,17 +24,17 @@ def _make_variable(_s, _l, tokens: ParseResults) -> Variable:  # type:ignore[no-
 
 subscript = Suppress("_{") + Word(nums)("subscript") + Suppress("}")
 variable_pe = Word(alphas, alphanums)("name") + Optional(subscript)
-variable_pe.setParseAction(_make_variable)
-variable_pe.setName("variable")
+variable_pe.set_parse_action(_make_variable)
+variable_pe.set_name("variable")
 
 
 def _unpack(_s, _l, tokens: ParseResults):  # type:ignore[no-untyped-def]
     return tokens[0]
 
 
-variables_pe = delimitedList(Group(variable_pe).setParseAction(_unpack))
-_children_pe = Group(variables_pe).setResultsName("children")
-_parents_pe = Group(Optional(Suppress("|") + variables_pe)).setResultsName("parents")
+variables_pe = DelimitedList(Group(variable_pe).set_parse_action(_unpack))
+_children_pe = Group(variables_pe).set_results_name("children")
+_parents_pe = Group(Optional(Suppress("|") + variables_pe)).set_results_name("parents")
 probability_pe = Suppress("P(") + _children_pe + _parents_pe + Suppress(")")
 
 
@@ -47,14 +47,14 @@ def _make_probability(_s, _l, tokens: ParseResults) -> Probability:  # type:igno
     )
 
 
-probability_pe.setParseAction(_make_probability)
-probability_pe.setName("probability")
+probability_pe.set_parse_action(_make_probability)
+probability_pe.set_name("probability")
 
 qfactor_pe = (
     Suppress("Q[\\{")
-    + Group(variables_pe).setResultsName("codomain")
+    + Group(variables_pe).set_results_name("codomain")
     + Suppress("\\}](")
-    + Group(variables_pe).setResultsName("domain")
+    + Group(variables_pe).set_results_name("domain")
     + Suppress(")")
 )
 
@@ -66,5 +66,5 @@ def _make_q(_s, _l, tokens: ParseResults) -> QFactor:  # type:ignore[no-untyped-
     )
 
 
-qfactor_pe.setParseAction(_make_q)
-qfactor_pe.setName("qfactor")
+qfactor_pe.set_parse_action(_make_q)
+qfactor_pe.set_name("qfactor")
