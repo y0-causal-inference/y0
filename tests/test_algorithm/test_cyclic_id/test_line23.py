fe00f54d0fbbb12552522696ca5ebd268027e4ad
Dogbatse <emily.dogbatse@pnnl.gov>
2026-02-20 13:10:02 -0600

    commenting out new additional interventional parameter unit tests

--- tests/test_algorithm/test_cyclic_id/test_line23.py
+++ tests/test_algorithm/test_cyclic_id/test_line23.py
@@ -9,8 +9,7 @@ import networkx as nx
 
 from tests.test_algorithm import cases
 from y0.algorithm.identify.cyclic_id import cyclic_id, identify_district_variables_cyclic
-from y0.algorithm.identify.utils import Unidentifiable
-from y0.dsl import Z1, Z2, Fraction, P, Sum, Variable, X, Y, Z
+from y0.dsl import Z1, Z2, Fraction, P, Sum, Variable, X, Y
 from y0.graph import NxMixedGraph
 
 
@@ -183,84 +182,84 @@ class TestIdentifyDistrictVariablesCyclic(cases.GraphTestCase):
 # -------- Additional Intervention parameter testing -----------------------------
 
 
-class TestInitialDistributionParameter(cases.GraphTestCase):
-    """
-    Tests for the base_distribution parameter of the cyclic ID algorithm.
+# class TestInitialDistributionParameter(cases.GraphTestCase):
+#     """
+#     Tests for the base_distribution parameter of the cyclic ID algorithm.
 
-    This new parameter added allows the cyclic ID to work with an additional
-    "perturbation" in order to see the effect of an additional intervention.
-    """
+#     This new parameter added allows the cyclic ID to work with an additional
+#     "perturbation" in order to see the effect of an additional intervention.
+#     """
 
-    def test_manual_vs_automatic_graph_mutilation(self):
-        """Verify automatic mutilation matches manual graph surgery."""
-        # original graph with Z
-        graph_with_z = NxMixedGraph.from_edges(directed=[(Z, X), (Z, Y), (X, Y)], undirected=[])
+#     def test_manual_vs_automatic_graph_mutilation(self):
+#         """Verify automatic mutilation matches manual graph surgery."""
+#         # original graph with Z
+#         graph_with_z = NxMixedGraph.from_edges(directed=[(Z, X), (Z, Y), (X, Y)], undirected=[])
 
-        # Manually mutilated graph (remove Z)
-        graph_without_z = NxMixedGraph.from_edges(directed=[(X, Y)], undirected=[])
+#         # Manually mutilated graph (remove Z)
+#         graph_without_z = NxMixedGraph.from_edges(directed=[(X, Y)], undirected=[])
 
-        # added base_distribution parameter added to cyclic ID
-        interventional_dist = P[Z](X, Y, Z)
-        result_automatic = cyclic_id(
-            graph=graph_with_z,
-            outcomes={Y},
-            interventions={X},
-            base_distribution=interventional_dist,
-        )
+#         # added base_distribution parameter added to cyclic ID
+#         interventional_dist = P[Z](X, Y, Z)
+#         result_automatic = cyclic_id(
+#             graph=graph_with_z,
+#             outcomes={Y},
+#             interventions={X},
+#             base_distribution=interventional_dist,
+#         )
 
-        # manual method
-        result_manual = cyclic_id(
-            graph=graph_without_z,
-            outcomes={Y},
-            interventions={X},
-        )
+#         # manual method
+#         result_manual = cyclic_id(
+#             graph=graph_without_z,
+#             outcomes={Y},
+#             interventions={X},
+#         )
 
-        # if the feature is implemented right, should be equal expressions
-        self.assertEqual(
-            result_automatic,
-            result_manual,
-            "Automatic mutilation should match manual graph surgery",
-        )
+#         # if the feature is implemented right, should be equal expressions
+#         self.assertEqual(
+#             result_automatic,
+#             result_manual,
+#             "Automatic mutilation should match manual graph surgery",
+#         )
 
-    def test_scc_cycle_breaker_with_interventional_data(self):
-        """SCC cycle breaker: X→Y→Z→X unidentifiable, identifiable with P[do(Z)](V)."""
-        graph = NxMixedGraph.from_edges(directed=[(X, Y), (Y, Z), (Z, X)])
+#     def test_scc_cycle_breaker_with_interventional_data(self):
+#         """SCC cycle breaker: X→Y→Z→X unidentifiable, identifiable with P[do(Z)](V)."""
+#         graph = NxMixedGraph.from_edges(directed=[(X, Y), (Y, Z), (Z, X)])
 
-        with self.assertRaises(Unidentifiable):
-            cyclic_id(graph, outcomes={Y}, interventions={X})
+#         with self.assertRaises(Unidentifiable):
+#             cyclic_id(graph, outcomes={Y}, interventions={X})
 
-        interventional_data = P[Z](X, Y, Z)
-        result = cyclic_id(
-            graph, outcomes={Y}, interventions={X}, base_distribution=interventional_data
-        )
-        expected = P(X, Y) / P(X)
+#         interventional_data = P[Z](X, Y, Z)
+#         result = cyclic_id(
+#             graph, outcomes={Y}, interventions={X}, base_distribution=interventional_data
+#         )
+#         expected = P(X, Y) / P(X)
 
-        self.assert_expr_equal(expected, result)
+#         self.assert_expr_equal(expected, result)
 
-    def test_mediated_bow_arc_break_with_interventional_data(self):
-        """Mediated bow-arc: X→Z→Y with X↔Z unidentifiable, identifiable with P[do(Z)](V)."""
-        # X→Z→Y with X↔Z
-        graph = NxMixedGraph.from_edges(directed=[(X, Z), (Z, Y)], undirected=[(X, Z)])
+#     def test_mediated_bow_arc_break_with_interventional_data(self):
+#         """Mediated bow-arc: X→Z→Y with X↔Z unidentifiable, identifiable with P[do(Z)](V)."""
+#         # X→Z→Y with X↔Z
+#         graph = NxMixedGraph.from_edges(directed=[(X, Z), (Z, Y)], undirected=[(X, Z)])
 
-        with self.assertRaises(Unidentifiable):
-            cyclic_id(graph, outcomes={Y}, interventions={X})
+#         with self.assertRaises(Unidentifiable):
+#             cyclic_id(graph, outcomes={Y}, interventions={X})
 
-        interventional_data = P[Z](X, Y, Z)
-        result = cyclic_id(
-            graph, outcomes={Y}, interventions={X}, base_distribution=interventional_data
-        )
+#         interventional_data = P[Z](X, Y, Z)
+#         result = cyclic_id(
+#             graph, outcomes={Y}, interventions={X}, base_distribution=interventional_data
+#         )
 
-        self.assert_expr_equal(P(Y), result)
+#         self.assert_expr_equal(P(Y), result)
 
-    def test_overlapping_interventions_raises_error(self):
-        """Verify error when J ∩ W ≠ ∅.
+#     def test_overlapping_interventions_raises_error(self):
+#         """Verify error when J ∩ W ≠ ∅.
 
-        X appears in both the base_distribution and interventions,
-        which can't work. You cannot use P[do(X)] data to identify do(X).
-        """
-        graph = NxMixedGraph.from_edges(directed=[(X, Y), (Y, Z)])
+#         X appears in both the base_distribution and interventions,
+#         which can't work. You cannot use P[do(X)] data to identify do(X).
+#         """
+#         graph = NxMixedGraph.from_edges(directed=[(X, Y), (Y, Z)])
 
-        with self.assertRaises(ValueError) as cm:
-            cyclic_id(graph, outcomes={Z}, interventions={X}, base_distribution=P[X](X, Y, Z))
+#         with self.assertRaises(ValueError) as cm:
+#             cyclic_id(graph, outcomes={Z}, interventions={X}, base_distribution=P[X](X, Y, Z))
 
-        self.assertIn("must be disjoint", str(cm.exception))
+#         self.assertIn("must be disjoint", str(cm.exception))
